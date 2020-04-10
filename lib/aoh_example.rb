fdbcd5426c989826f5faecf188f8545229964fc9
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 19:09:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,14 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  arra = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G,
+  ]
+
 end
 
 def aoh_lookup(aoh, row, key)
